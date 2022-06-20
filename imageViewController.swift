180b5c58232d6a9dafbe313a87c7ba3837f21299
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-20 21:58:28 +0300

    ImageViewController eklendi

---
+++ imageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  imageViewController.swift
+//  LandmarkBook
+//
+//  Created by Fulya Sarıyar on 20.06.2022.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
